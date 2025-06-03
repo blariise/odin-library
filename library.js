4126157908edaff18adfa08e46d0f386f9502643
blarrise <61951559+blariise@users.noreply.github.com>
2025-06-03 17:28:37 +0200

    Add delete button to cards

--- library.js
+++ library.js
@@ -41,19 +41,37 @@ function displayLibrary() {
     is_readed.className = book.is_readed;
     is_readed.textContent = book.is_readed ? "Completed" : "Not completed";
 
-    const id = document.createElement("div");
-    id.className = "id";
-    id.textContent = book.id;
+    card.dataset.id = book.id;
 
+    const remove_button = document.createElement("button");
+    remove_button.className = "remove_button";
+    const remove_button_svg = generateDeleteButtonIcon();
+    remove_button.appendChild(remove_button_svg);
+
+    card.appendChild(remove_button);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
     card.appendChild(is_readed);
-    card.appendChild(id);
 
     cards_container.appendChild(card);
   });
 }
+
+function generateDeleteButtonIcon() {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", `M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,
+    12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,
+    8 0 0,0 12,4M16,10V17A1,1 0 0,1 15,18H9A1,1 0 0,1 8,
+    17V10H16M13.5,6L14.5,7H17V9H7V7H9.5L10.5,6H13.5Z`
+  );
+  svg.appendChild(path);
+  return svg;
+}
+
 
 function addBookDialogListener() {
   const show_dialog = document.querySelector(".open-add-book-dialog");
@@ -86,4 +104,26 @@ function addBookDialogListener() {
   
 }
 
+function addRemoveBookButtonListener() {
+  const cards = document.querySelector(".cards");
+  cards.addEventListener("click", (e) => {
+    let target = e.target.closest("button");
+    let book_id = e.target.closest("div").dataset.id;
+    if (target !== null) {
+      const card_id = target.closest("div").dataset.id;
+      for (let i = 0; i < my_library.length; ++i) {
+        if (my_library[i].id === card_id) {
+          my_library.splice(i, 1);
+          displayLibrary();
+        }
+      }
+    }
+  });
+}
+
+const temp = new Book("Harry Potter", "J.K. Rolling", 1337, true);
+addBookToLibrary(temp);
+displayLibrary();
+
 addBookDialogListener();
+addRemoveBookButtonListener();
